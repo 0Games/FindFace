bb8c65547cbea9ff3dec252da214a834b9227da8
JediAlejandro <alejandrocg1015@gmail.com>
2017-08-09 09:01:29 -0700

    Color link

--- js/animations.js
+++ js/animations.js
@@ -4,3 +4,17 @@ $(document).ready(function(){
       $("#p_email").fadeToggle(300);
     });
 });
+
+/* Material Design Ripple - x */
+$(document).ready(function() {
+  $('body').on('click touchstart', '.card', function(e) {
+       var ripple = $(this).find(".ripple");
+       ripple.removeClass("animate");
+       var x = parseInt(e.pageX - $(this).offset().left) - (ripple.width() / 2);
+       var y = parseInt(e.pageY - $(this).offset().top) - (ripple.height() / 2);
+       ripple.css({
+          top: y,
+          left: x
+       }).addClass("animate");
+  });
+});
