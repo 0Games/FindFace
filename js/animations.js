9a8101363f3b4467121b77366ad036bda75d1016
JediAlejandro <alejandrocg1015@gmail.com>
2017-08-09 10:29:26 -0700

    Adding ripple effect for card (remove position relative)

--- js/animations.js
+++ js/animations.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
 /* Material Design Ripple - Cards */
 $(document).ready(function() {
-  $('body').on('click touchstart', 'a', function(e) {
+  $('body').on('click touchstart', '.card', function(e) {
        var ripple = $(this).find(".ripple");
        ripple.removeClass("animate");
        var x = parseInt(e.pageX - $(this).offset().left) - (ripple.width() / 2);
